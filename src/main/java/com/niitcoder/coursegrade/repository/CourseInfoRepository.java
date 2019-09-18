c3080a169b3ecaacf8231cb3e08885124c488234
y9786 <978623783@qq.com>
2019-09-18 17:20:11 +0800

    1组部分接口

--- src/main/java/com/niitcoder/coursegrade/repository/CourseInfoRepository.java
+++ src/main/java/com/niitcoder/coursegrade/repository/CourseInfoRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 /**
  * Spring Data  repository for the CourseInfo entity.
@@ -14,4 +16,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CourseInfoRepository extends JpaRepository<CourseInfo, Long> {
     Page<CourseInfo> findByCourseUser(String login, Pageable page);
+    /**
+     * 根据学生名查找课程信息
+     * @param student
+     * @return
+     */
+    @Query(nativeQuery = true, value = "SELECT * FROM course_info WHERE id =(\n" +
+        "            SELECT course_id FROM course_group WHERE id=(\n" +
+        "            SELECT group_id FROM student_course_group WHERE student=?1))")
+    List<CourseInfo> findByStudent(String student);
 }
