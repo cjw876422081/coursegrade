a892323b42f42dd3f75eb1b548fdbff6aa41e9de
软件1721李漷方 <2895844821@qq.com>
2019-09-18 14:08:32 +0800

    授课内容：查询单个课程里的全部授课内容（树结构）

--- src/main/java/com/niitcoder/coursegrade/service/impl/CoursePlanServiceImpl.java
+++ src/main/java/com/niitcoder/coursegrade/service/impl/CoursePlanServiceImpl.java
@@ -1,16 +1,28 @@
 package com.niitcoder.coursegrade.service.impl;
 
+import com.alibaba.fastjson.util.TypeUtils;
+import com.niitcoder.coursegrade.domain.CourseInfo;
+import com.niitcoder.coursegrade.repository.CourseInfoRepository;
 import com.niitcoder.coursegrade.service.CoursePlanService;
 import com.niitcoder.coursegrade.domain.CoursePlan;
 import com.niitcoder.coursegrade.repository.CoursePlanRepository;
+import com.niitcoder.coursegrade.service.dto.CourseInfoPlan;
+import com.niitcoder.coursegrade.service.dto.CoursePlanDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -24,8 +36,14 @@ public class CoursePlanServiceImpl implements CoursePlanService {
 
     private final CoursePlanRepository coursePlanRepository;
 
-    public CoursePlanServiceImpl(CoursePlanRepository coursePlanRepository) {
+    private final CourseInfoRepository courseInfoRepository;
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public CoursePlanServiceImpl(CoursePlanRepository coursePlanRepository, CourseInfoRepository courseInfoRepository, JdbcTemplate jdbcTemplate) {
         this.coursePlanRepository = coursePlanRepository;
+        this.courseInfoRepository = courseInfoRepository;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     /**
@@ -77,4 +95,89 @@ public class CoursePlanServiceImpl implements CoursePlanService {
         log.debug("Request to delete CoursePlan : {}", id);
         coursePlanRepository.deleteById(id);
     }
+
+    @Override
+    public CourseInfoPlan getCourseInfoPlan(Long courseId) {
+        CourseInfo courseInfo = new CourseInfo();
+        courseInfo = courseInfoRepository.findById(courseId).get();
+        CourseInfoPlan courseInfoPlan = new CourseInfoPlan();
+        courseInfoPlan.setId(courseInfo.getId());
+        courseInfoPlan.setCourseCode(courseInfo.getCourseCode());
+        courseInfoPlan.setCourseName(courseInfo.getCourseName());
+        courseInfoPlan.setCourseCount(courseInfo.getCourseCount());
+        courseInfoPlan.setCourseWeekCount(courseInfo.getCourseWeekCount());
+        courseInfoPlan.setCourseMemo(courseInfo.getCourseMemo());
+        courseInfoPlan.setDataTime(courseInfo.getDataTime());
+        courseInfoPlan.setCourseUser(courseInfo.getCourseUser());
+
+        List<CoursePlanDTO> coursePlanDTOList = new ArrayList<>();
+        coursePlanDTOList = getCoursePlanDTOByCourseId(courseInfoPlan.getId());
+        if (coursePlanDTOList.isEmpty()) {
+            courseInfoPlan.setLeaf(true);
+        } else {
+            courseInfoPlan.setLeaf(false);
+            courseInfoPlan.setChildren(coursePlanDTOList);
+        }
+
+        return courseInfoPlan;
+    }
+
+    @Override
+    public List<CoursePlan> getPlanByParentId(Long id) {
+        List<CoursePlan> coursePlanList = new ArrayList<>();
+        coursePlanList = coursePlanRepository.findByParentPlanId(id);
+        return coursePlanList;
+    }
+
+    @Override
+    public List<CoursePlanDTO> getCoursePlanDTOByCourseId(Long courseId) {
+        String sql = "SELECT a.*,(SELECT COUNT(1) FROM course_info b WHERE b.id=a.course_id) as leaf FROM course_plan a WHERE a.course_id = " + courseId;
+        List<Map<String, Object>> sqlResults = jdbcTemplate.queryForList(sql);
+        List<CoursePlanDTO> result = new ArrayList<>();
+        for (Map<String, Object> sqlResult : sqlResults) {
+            CoursePlanDTO coursePlanDTO = new CoursePlanDTO();
+            coursePlanDTO.setId(TypeUtils.castToLong(sqlResult.get("id")));
+            coursePlanDTO.setpId(TypeUtils.castToLong(sqlResult.get("parent_plan_id")));
+            coursePlanDTO.setLeaf(TypeUtils.castToInt(sqlResult.get("leaf")) > 0 ? false : true);
+            coursePlanDTO.setPlanMemo(TypeUtils.castToString(sqlResult.get("plan_memo")));
+            coursePlanDTO.setPlanTarget(TypeUtils.castToString(sqlResult.get("plan_target")));
+            String time = TypeUtils.castToString(sqlResult.get("data_time"));
+            time = time.replace(".0", ".000Z").replace(" ", "T");
+            coursePlanDTO.setPlanTime(ZonedDateTime.parse(time, DateTimeFormatter.ISO_OFFSET_DATE_TIME.withZone(ZoneId.systemDefault())));
+            coursePlanDTO.setPlanCount(TypeUtils.castToInt(sqlResult.get("plan_count")));
+            coursePlanDTO.setCourse(TypeUtils.castToLong(sqlResult.get("course_id")));
+            if (!coursePlanDTO.isLeaf()) {
+                coursePlanDTO.setChildren(getSubPlan(coursePlanDTO.getId()));
+            }
+            result.add(coursePlanDTO);
+        }
+        return result;
+    }
+
+    @Override
+    public List<CoursePlanDTO> getSubPlan(Long pid) {
+        String sql = "select a.*,(select count(1) from course_plan b where b.parent_plan_id=a.id) as leaf from course_plan a where parent_plan_id=" + pid;
+        List<Map<String, Object>> sqlResults = jdbcTemplate.queryForList(sql);
+        List<CoursePlanDTO> result = new ArrayList<>();
+        for (Map<String, Object> sqlResult : sqlResults) {
+            CoursePlanDTO coursePlanDTO = new CoursePlanDTO();
+            coursePlanDTO.setId(TypeUtils.castToLong(sqlResult.get("id")));
+            coursePlanDTO.setpId(TypeUtils.castToLong(sqlResult.get("parent_plan_id")));
+            coursePlanDTO.setLeaf(TypeUtils.castToInt(sqlResult.get("leaf")) > 0 ? false : true);
+            coursePlanDTO.setPlanMemo(TypeUtils.castToString(sqlResult.get("plan_memo")));
+            coursePlanDTO.setPlanTarget(TypeUtils.castToString(sqlResult.get("plan_target")));
+            String time = TypeUtils.castToString(sqlResult.get("data_time"));
+            time = time.replace(".0", ".000Z").replace(" ", "T");
+            coursePlanDTO.setPlanTime(ZonedDateTime.parse(time, DateTimeFormatter.ISO_OFFSET_DATE_TIME.withZone(ZoneId.systemDefault())));
+            coursePlanDTO.setPlanCount(TypeUtils.castToInt(sqlResult.get("plan_count")));
+            coursePlanDTO.setCourse(TypeUtils.castToLong(sqlResult.get("course_id")));
+            if (!coursePlanDTO.isLeaf()) {
+                coursePlanDTO.setChildren(getSubPlan(coursePlanDTO.getId()));
+            }
+            result.add(coursePlanDTO);
+        }
+        return result;
+    }
+
+
 }
